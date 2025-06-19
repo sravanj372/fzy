fbb40b225ebcd13d3b8fda668dcbb90e08b1d1b3
Sanjeev <sanjeev.ksv@gmail.com>
2025-06-19 10:04:49 +0530

    profile

--- src/adminflow/Profile.tsx
+++ src/adminflow/Profile.tsx
@@ -1,46 +1,70 @@
-import { Box, Button, Paper, TextField, Typography } from "@mui/material"
-import { Stack } from "@mui/system"
-import { useState } from "react"
+import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
+import { useState } from "react";
 
 const Profile = () => {
-   
-  const profiledata=[
-    {id:1,label:'Phone Number',value:'+612303'},
-    {id:2,label:'Email',value:'Ramesh@gmail.com'},
-    {id:3,label:'Password',value:'*******'},
-]
+  const profiledata = [
+    { id: 1, label: "Phone Number", value: "+612303" },
+    { id: 2, label: "Email", value: "Ramesh@gmail.com" },
+    { id: 3, label: "Password", value: "*******" },
+];
 
-const[state,setState]=useState(false)
+  const [state, setState] = useState(false);
 
-const[email,setEmail]=useState('')
-const updateEmailhandler=(profile:any)=>{
+  const [email, setEmail] = useState("");
+
+  const updateEmailhandler = (profile: any) => {
+    if (profile.id === 2) {
+      setState(true);
+      setEmail(profile.value);
+    }
+  };
+
  
-  if(profile.id ===2){
-    setState(true)
-    setEmail(profile.value)
-}
-}
+ const emailchangeHandler=(event:any)=>{
+     
+   setEmail(event.target.value)
+ } 
   return (
-  <Box padding={1} gap={2}>  
-      <Paper  sx={{border: "1px solid green",overflowX:{xs:'hidden'}}}>
-        <Typography color="#2F7A52" pl={2}>Profile</Typography>
-        <Typography fontWeight="700" color="#2F7A52" pl={2} mt={2}>ADMIN</Typography>
-         {profiledata.map((profile)=>(
-           <Stack direction="row" gap={2} p={2} display="flex" flexWrap="wrap">
+    <Box padding={1} gap={2}>
+      <Paper sx={{ border: "1px solid green", overflowX: { xs: "hidden" } }}>
+        <Typography color="#2F7A52" pl={2}>
+          Profile
+        </Typography>
+        <Typography fontWeight="700" color="#2F7A52" pl={2} mt={2}>
+          ADMIN
+        </Typography>
+        {profiledata.map((profile) => (
+          <Stack direction="row" 
+          key={profile.id} gap={2} p={2} display="flex" flexWrap="wrap">
             <Typography minWidth="150px">{profile.label}</Typography>
-            {state===true && profile.id===2 ?<TextField size="small" value={email} />:<Typography>{profile.value}</Typography> }
-            {profile.label==='Email'&&
-            <Typography sx={{textDecoration:'underline',color:'#2F7A52',cursor:'pointer'}} 
-              onClick={()=>updateEmailhandler(profile)}>Update</Typography>}
-            {profile.label==='Password'&& <Button variant="outlined" size="small">Change Password</Button>} 
-           </Stack>
-
-          ))}
-          
-
+            {state === true && profile.id === 2 ? (
+              <TextField size="small" value={email} onChange={emailchangeHandler}/>
+            ) : (
+              <Typography>{profile.value}</Typography>
+            )}
+            {profile.label === "Email"&&(
+              <Typography
+                sx={{
+                  textDecoration: "underline",
+                  color: "#2F7A52",
+                  cursor: "pointer",
+                }}
+                onClick={() => updateEmailhandler(profile)}
+              >
+                Edit
+              </Typography>
+            )}
+             {profile.label === "Password" && (
+              <Button variant="outlined" size="small">
+                Change Password
+              </Button>
+            )}
+          </Stack>
+        ))}
       </Paper>
-   </Box>   
-  )
-}
+    </Box>
+  );
+};
 
-export default Profile
+export default Profile;
